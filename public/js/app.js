6709682275e8c741ec56974152471ac11be2dd97
Bo Skakke <bo@skakke.dk>
2019-11-03 13:58:49 +0100

    images-folder

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/bo/code/test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/bo/code/test/resources/sass/main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! /Users/bo/code/bobedre/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/bo/code/bobedre/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
